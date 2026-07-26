3314018d57eea7708a2141d9454121ecda3bdc61
German Reyes <oswaldo101.gr@gmail.com>
2026-07-25 20:58:10 -0600

    add initial project structure with Maven configuration and Spring Boot application

---
+++ hexchest/src/main/java/com/osweld/hexchest/HexchestApplication.java
@@ -0,0 +1,13 @@
+package com.osweld.hexchest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexchestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexchestApplication.class, args);
+	}
+
+}
