bbf6fc7a90ce54c5a1079d8b5eab9e87daca3309
German Reyes <oswaldo101.gr@gmail.com>
2026-07-31 21:39:32 -0600

    feat: add AbstractIntegrationTest class for PostgreSQL integration testing

---
+++ hexchest/src/test/java/com/osweld/hexchest/AbstractIntegrationTest.java
@@ -0,0 +1,25 @@
+package com.osweld.hexchest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
+@SpringBootTest
+public abstract class AbstractIntegrationTest {
+
+    static final PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:16-alpine");
+
+    static {
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+
+}
